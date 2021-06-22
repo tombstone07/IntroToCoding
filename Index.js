59964b69a903ebf9f350e0b3ba92d2a390d407b7
tombstone07 <61301282+tombstone07@users.noreply.github.com>
2021-06-22 11:07:28 +0000

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,9 +1,9 @@
-// console.log("Hello World\n");
-// alert("Hi Bumi");
+console.log("Hello World\n");
+alert("Hi Bumi");
 
-// var userName = prompt("Please enter your name");
-// var userAge = prompt("Please enter your age");
-// console.log("Hello " + userName + " .You are " + userAge*128*52 + " weeks old.\n");
+var userName = prompt("Please enter your name");
+var userAge = prompt("Please enter your age");
+console.log("Hello " + userName + " .You are " + userAge*128*52 + " weeks old.\n");
 
 var myArray;
 myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], 10, 11, 12];
@@ -11,4 +11,4 @@ var myData;
 myData = myArray[0][1];
 myArray.push[[13, 14, 15], 16, 17, 18];
 var myArray;
-// console.log(myData + " \n");
+console.log(myData + " \n");
